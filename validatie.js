9662da9f8a79754e0c68c470708cbb72fb4ea6d1
Felix Bastijns <r0295639@student.thomasmore.be>
2023-03-02 10:59:23 +0100

    Eerste versie javascript + Lay out formulier bijwerken

---
+++ validatie.js
@@ -0,0 +1,59 @@
+let error = [];
+
+//Bij het drukken op de registreer knop zal de functie validateForm uitgevoerd worden
+
+document.getElementById("registreer").addEventListener("click", validateForm);
+
+
+
+document.getElementById("errors").hidden = true;
+document.getElementById("succes").hidden = true;
+document.getElementById("betalingsInfo").hidden = true;   
+
+
+
+function checkEmptyField(veld, melding) {
+  if (veld.value.trim() == "") error.push(melding);
+}
+
+function validateForm() {
+    event.preventDefault();
+
+    error = [];
+  //Variabelen declareren en elementen inladen
+  let voornaam = document.getElementById('voornaam');
+  let naam = document.getElementById('naam');
+  let gebruikersnaam = document.getElementById('gebruikersnaam');
+  let email = document.getElementById('email');
+  let wachtwoord = document.getElementById('wachtwoord');
+  let herhaalWachtwoord = document.getElementById('herhaalWachtwoord');
+  let adres = document.getElementById('adres');
+  let land = document.getElementById('land');
+  let provincie = document.getElementById('provincie');
+  let postcode = document.getElementById('postcode');
+  let nieuwsbrief = document.getElementById('nieuwsbrief');
+  let voorwaarden = document.getElementById('voorwaarden');
+  let betaling = document.getElementsByName('betalingswijze');
+
+  checkEmptyField(voornaam, "Het veld voornaam is vereist");
+  checkEmptyField(naam, "Het veld naam is vereist");
+  checkEmptyField(gebruikersnaam, "Het veld gebruikersnaam is vereist");
+  checkEmptyField(email, "Het veld email is vereist");
+  checkEmptyField(adres, "Het veld wachtwoord is vereist");
+  checkEmptyField(wachtwoord, "Het veld herhaal wachtwoord is vereist");
+  checkEmptyField(herhaalWachtwoord, "Het veld adres is vereist");
+  checkEmptyField(land, "Gelieve een land te selecteren");
+  checkEmptyField(provincie, "Gelieve een provincie te selecteren");
+
+  if (error.length == 0) {
+    document.getElementById("succes").hidden = false;
+    document.getElementById("betalingsInfo").hidden = false;
+    document.getElementById("errors").hidden = true;    
+  }else{
+    document.getElementById("errors").hidden = false;
+    document.getElementById("succes").hidden = true;
+    document.getElementById("betalingsInfo").hidden = true;
+  }
+}
+
+
